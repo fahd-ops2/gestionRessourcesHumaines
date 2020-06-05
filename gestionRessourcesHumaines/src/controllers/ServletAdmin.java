098408fa4f9d2d983f77aed7488083386d757265
private <private@192.168.1.7>
2020-06-05 23:15:57 +0000

    last

--- gestionRessourcesHumaines/src/controllers/ServletAdmin.java
+++ gestionRessourcesHumaines/src/controllers/ServletAdmin.java
@@ -26,14 +26,14 @@ public class ServletAdmin extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("done");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
