ee35bc872bb50fd7d93e67d41fdad4e693540568
private <private@192.168.1.7>
2020-06-08 02:42:33 +0000

    jsp servlet

--- gestionRessourcesHumaines/src/DAO/PersonnelDao.java
+++ gestionRessourcesHumaines/src/DAO/PersonnelDao.java
@@ -30,14 +30,14 @@ public class PersonnelDao {
 			 pst.setString(2,p.getCin());
 			 pst.setString(3,p.getPren_n_arabe());
 			 pst.setString(4,p.getPren_n());
-			 pst.setDate(5,p.getDatenaissance());
+			 pst.setString(5,p.getDatenaissance());
 			 pst.setString(6,p.getAdresse());
 			 pst.setString(7,p.getTel());
 		     pst.setString(8,p.getNationalite());
 			 pst.setString(9,p.getSexe());
 			 return pst.executeUpdate();
 		}catch(Exception e ){
-			System.out.println("PersonnelDao not done "); 
+			System.out.println("PersonnelDaoinsert not done "); 
 			return -1;
 		}
 	
@@ -46,18 +46,18 @@ public class PersonnelDao {
 		try{
 			String req="UPDATE `personnel` SET `Dateembauche`=?,`Echelle`=?,`Echelon`=?,`Situationfamiliale`=?,`NbreEnfant`=?,`Marieemploye`=?,`Num_tE`=?,`DateDésignation`=? WHERE ID=?";
 			 PreparedStatement pst= cna.prepareStatement(req);
-			 pst.setDate(1,p.getDateembauche());
+			 pst.setString(1,p.getDateembauche());
 			 pst.setString(2,p.getEchelle());
 			 pst.setString(3,p.getEchelon());
 			 pst.setString(4,p.getSituationfamiliale());
 			 pst.setInt(5,p.getNbreEnfant());
 			 pst.setString(6,p.getMarieemploye());
 			 pst.setString(7,p.getNum_tE());
-		     pst.setDate(8,p.getDateDesignation());
+		     pst.setString(8,p.getDateDesignation());
 			 pst.setInt(9,p.getID());
 			 return pst.executeUpdate();
 		}catch(Exception e ){
-			System.out.println("PersonnelDao not done "); 
+			System.out.println("PersonnelDaosuivi not done "); 
 			return -1;
 		}
 	
@@ -140,13 +140,14 @@ public class PersonnelDao {
 		try{
 	    Date d = new Date(0);
 		//System.out.println(d);
-		// insert //personne= new Personnel("3","3","fahd","fa", d,"adresse","065656","marocain","masculin");
+		 //personne= new Personnel("3","3","fahd","fa", "10/11/2000","adresse","065656","marocain","masculin");
 		//System.out.println(personne.toString());
-		PersonnelDao pdao=new PersonnelDao();
+		//PersonnelDao pdao=new PersonnelDao();
+		//pdao.insertPersonnel(personne);
 		//pdao.insertPersonnel(personne);
 		//pdao.deletePersonnel(1);
 		//suivi insertion 
-		//personne=new Personnel(1,2,"aa","aa","aa","aa","aa",d,d);
+		//personne=new Personnel(2,3,"aa","aa","aa","aa","aa",d,d);
 		//pdao.suiviinsertPersonnel(personne);
 		//personne= new Personnel(1, 3, "fahda","fahda", "fahda", "fahd", "fahda","fahda", "fahda", "fahda","fahda", "fahda");
 		//System.out.println(personne.toString());
@@ -155,12 +156,12 @@ public class PersonnelDao {
 			//PersonnelDao ss= new PersonnelDao() ;
 			//ArrayList<Personnel> pp =ss.SelectAll();
 			//System.out.println(pp.toString());
-		ResultSet res =pdao.selectby("qq");
+	    /*ResultSet res =pdao.selectby("qq");
 		
 	    while (res.next()){
 	    	System.out.println(res.getInt(1)+" "+res.getInt(15)+" "+res.getString(2)+" "+res.getString(3)+" "+res.getString(4)+" "+res.getString(5)+" "+res.getDate(11)+" "+res.getString(7)+" "+res.getString(8)+" "+res.getString(9)+" "+res.getString(10)+" "+res.getString(12)+" "+res.getString(13)+" "+res.getString(14)+" "+res.getString(16)+" "+res.getString(17)+" "+res.getDate(18)+" "+res.getDate(6));
 	    	
-	    }
+	    }*/
 		System.out.println("done");}
 		catch(Exception e){
 			System.out.println("not done");}
